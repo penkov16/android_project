99f4503a2f1a2a77c6b6a735a2829b581eeaba20
Alexandr <penkov-sasha@mail.ru>
2023-11-01 13:07:16 +0300

    Комментарий

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 5;
+        double c = a + 3
+        Scanner scan=
+
+    }
